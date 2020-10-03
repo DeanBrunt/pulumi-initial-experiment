cd7ffcd5255ac65b58545fb5c12111ab777d2389
Dean Brunt <dean.brunt@ziglu.io>
2020-10-03 17:33:22 +0100

    Modularise project creation

--- index.ts
+++ index.ts
@@ -1,33 +1,13 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as gcp from "@pulumi/gcp";
-import * as random from "@pulumi/random";
+import { newGCPProject } from "./lib/gcp-project";
 import { ProjectService } from "./lib/gcp/services";
 
-const randomSuffix = new random.RandomPassword("project-suffix", {
-    special: false,
-    length: 8,
-    upper: false,
-    number: false,
+const project = newGCPProject("pulumi-experiment", {
+    name: `${pulumi.getStack()}-pulumi-experiment`,
+    services: [
+        ProjectService.CloudBilling,
+        ProjectService.CloudResourceManager,
+    ],
 });
-const projectName = `${pulumi.getStack()}-pulumi-experiment`;
-
-const project = new gcp.organizations.Project("pulumi-experiment-gcp-project", {
-    name: projectName,
-    projectId: pulumi.interpolate `${projectName}-${randomSuffix.result}`,
-});
-
-const servicesToEnable = [
-    ProjectService.CloudBilling,
-    ProjectService.CloudResourceManager,
-];
-
-servicesToEnable.map(
-    svc => {
-        new gcp.projects.Service(`pulumi-experiment-gcp-project-service-${svc}`, {
-            project: project.projectId,
-            service: svc,
-        })
-    }
-);
 
 export const projectId = project.projectId;
